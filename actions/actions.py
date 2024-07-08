be0d58a8e5daceac82fb30b57eed640b4f007dd4
whosarian <hartlarian@gmail.com>
2024-07-08 13:53:54 +0200

    refactor: Improve action for processing multiple questions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,70 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# actions.py
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import re
 
+class ActionProcessMultipleQuestions(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:
+        return "action_process_multiple_questions"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        message = tracker.latest_message.get('text')
+        sentences = re.split(r'[.!?]', message)
+
+        intent_recognized = False
+
+        for sentence in sentences:
+            sentence = sentence.strip()
+            if sentence:
+                if re.search(r'aps', sentence, re.IGNORECASE) and re.search(r'hand it in after the deadline', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_ask_weather")
+                    intent_recognized = True
+                elif re.search(r'apply for more than one program', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_application_many_programs_possible")
+                    intent_recognized = True
+                elif re.search(r'increase my chances', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_application_many_programs_chance_increase")
+                    intent_recognized = True
+                elif re.search(r'hand in GRE instead of GMAT', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_gre_instead_gmat")
+                    intent_recognized = True
+                elif re.search(r'GMAT mandatory', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_gmat_needed")
+                    intent_recognized = True
+                elif re.search(r'check my eligibility', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_eligibility_check")
+                    intent_recognized = True
+                elif re.search(r'who can check my eligibility', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_eligibility_check_who")
+                    intent_recognized = True
+                elif re.search(r'defer my study start date', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_defer_study_start")
+                    intent_recognized = True
+                elif re.search(r'semester start', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_semester_start")
+                    intent_recognized = True
+                elif re.search(r'how long is a typical semester', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_time_span_semester")
+                    intent_recognized = True
+                elif re.search(r'Do I need GMAT', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_gmat_needed")
+                    intent_recognized = True
+                elif re.search(r'Does HWR give out GMAT tests', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_hwr_gmat_test")
+                    intent_recognized = True
+                elif re.search(r'what kind of english certificate do I need', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_english_certificate")
+                    intent_recognized = True
+                elif re.search(r'Is duolingo accepted', sentence, re.IGNORECASE):
+                    dispatcher.utter_message(response="utter_duolingo_accepted")
+                    intent_recognized = True
+
+        if not intent_recognized:
+            dispatcher.utter_message(response="utter_please_rephrase")
+
+        return []
